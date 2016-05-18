417cf0504d33bade096f0ac9f57a84bb9c442d70
chanjoolee <grechan@lycos.co.kr>
2016-05-18 10:13:12 +0900

    파일옮기기테스트전

--- hynix/performance/iometer/iometer.py
+++ hynix/performance/iometer/iometer.py
@@ -24,21 +24,21 @@ def insertQuery(result):
                     MEASURE)
                 VALUES 
                     (:1, :2, :3, :4, :5, :6, :7, :8)'''
-
+ 
     try:
         cur.prepare(insert)
         cur.executemany(None, result)
 #         con.commit()
-        
+         
     except:
         pass
-
-
+ 
+ 
 def data_source(path):
     data_src = os.path.basename(path)
     return data_src
-
-
+ 
+ 
 def lists(folder_name, tool, data_src, _spec, _field, _measure):
     result = [(folder_name, 
                tool,     
@@ -48,33 +48,33 @@ def lists(folder_name, tool, data_src, _spec, _field, _measure):
                '1',      
                _field,   
                _measure)]
-
+ 
     insertQuery(result)
     print result
-         
- 
+          
+  
 def parseXml(folder_name, tool, file_name, tag_name):
-    
-    data_src = data_source(file_name)
      
+    data_src = data_source(file_name)
+      
     tree = ET.parse(file_name)
     root = tree.getroot()
     tag_text = root.findtext(tag_name)
     tag_val = tag_text.split(' ')[0]
     value = tag_name.split('/')  
-    
+     
     if value[0] == 'SeqTest':
         lists(folder_name, tool, data_src, _spec=value[0], _field=value[1], _measure=tag_val)
     elif value[0] == 'Random4K1TTest':
         lists(folder_name, tool, data_src, _spec=value[0], _field=value[1], _measure=tag_val)
     elif value[0] == 'Random4K64TTest':
         lists(folder_name, tool, data_src, _spec=value[0], _field=value[1], _measure=tag_val)
-        
-
+         
+ 
 def parseAccTime(folder_name, tool, file_name, tag_name):
-    
+     
     data_src = data_source(file_name)
-        
+         
     tree = ET.parse(file_name)
     root = tree.getroot()
     tag_text = root.findtext(tag_name)
@@ -82,61 +82,61 @@ def parseAccTime(folder_name, tool, file_name, tag_name):
     _float = float(tag_val)
     _float3 = '{0:.3f}'.format(_float)
     value = tag_name.split('/')
-    
+     
     lists(folder_name, tool, data_src, _spec=value[0], _field='-', _measure=_float3)
-
-
+ 
+ 
 def parseMark8Score(folder_name, tool, file_name, tag_name):
-    
+     
     data_src = data_source(file_name)
-    
+     
     tree = ET.parse(file_name)
     root = tree.getroot()
     tag_text = root.findtext(tag_name)
     tag_val = tag_text.split(' ')[0]
     value = tag_name.split('/')
-    
+     
     lists(folder_name, tool, data_src, _spec=value[2], _field='-', _measure=tag_val)
-    
-    
+     
+     
 def parseMark8(folder_name, tool, file_name, tag_name):
-    
+     
     data_src = data_source(file_name)
-    
+     
     tree = ET.parse(file_name)
     root = tree.getroot()
     tag_text = root.findtext(tag_name)
     tag_val = tag_text.split(' ')[0]
     val = int(tag_val)/1024/1024
     value = tag_name.split('/')
-    
+     
     lists(folder_name, tool, data_src, _spec=value[2], _field='-', _measure=val)
-    
-
+     
+ 
 def parseFdr(folder_name, tool, file, overall_tag, scenario_tag, overallperformance_tag):
-    
+     
     data_src = data_source(file)
-        
+         
     tree = ET.parse(file)
     root = tree.getroot()
     tag_text = root.findtext(overallperformance_tag)
-    
+     
     for overall in root.iter(overall_tag):
         for scenario in overall.iter(scenario_tag):
             lists(folder_name, tool, data_src, _spec=scenario.attrib['name'], _field='-', _measure=scenario.attrib['performance'])   
     lists(folder_name, tool, data_src, _spec='overallperformance', _field='-', _measure=tag_text)
-    
-
+     
+ 
 def parseTxt(folder_name, tool, file, line1, line2, spec, p):
-    
+     
     data_src = data_source(file)        
-        
+         
     with open(file) as f:
         for i, j in enumerate(f):
             pass
-    
+     
     num = i + 1
-        
+         
     with open(file) as f:        
         for line in itertools.islice(f, num-line1, num-line2):
             if p == pattern_txt: 
@@ -145,18 +145,18 @@ def parseTxt(folder_name, tool, file, line1, line2, spec, p):
                     data = match.groupdict()['flt']
                     raw_data = '{0:.2f}'.format(float(data))
                     lists(folder_name, tool, data_src, _spec=spec, _field='-', _measure=raw_data)
-    
+     
             elif p == pattern_score:
                 match = p.search(line)
                 if match:
                     score_data = match.groupdict()['score']
                     lists(folder_name, tool, data_src, _spec=spec, _field='-', _measure=score_data)
-
-
+ 
+ 
 def parseTxtPor(folder_name, tool, file, pattern, spec):
-    
+     
     data_src = data_source(file)
-    
+     
     with open(file) as f:
         for line in f:
             match = pattern.search(line)
@@ -164,23 +164,23 @@ def parseTxtPor(folder_name, tool, file, pattern, spec):
                 data = match.groupdict()['por']
                 value = int(data)/1000
                 lists(folder_name, tool, data_src, _spec=spec, _field='tBOOTUP', _measure=value)
-                
-            
+                 
+             
 def parseXlsx(folder_name, tool, file, pattern, race=file):
-    
+     
     data_src = data_source(file)
-    
+     
     wb = load_workbook(filename=file)
-      
+       
     if race == CDM301:
         sheet_ranges = wb['Sheet1']
     elif race == CDM503:
         sheet_ranges = wb['Sheet1']
     elif race == PCMARK_VANTAGE:
         sheet_ranges = wb['Result 1']
-    
+     
     pcmark_lists = [140, 142, 144, 146, 148, 150, 152, 154, 139]
-    
+     
     if race == CDM301:
         for i in xrange(7,15):
             match = pattern.search(sheet_ranges['A' + str(i)].value)              
@@ -201,7 +201,7 @@ def parseXlsx(folder_name, tool, file, pattern, race=file):
                     lists(folder_name, tool, data_src, _spec='Random', _field='Read 4KB (QD=32)', _measure=match.groupdict()['num'])
                 elif i == 14:
                     lists(folder_name, tool, data_src, _spec='Random', _field='Write 4KB (QD=32)', _measure=match.groupdict()['num'])                    
-              
+               
     elif race == CDM503:
         for i in xrange(8,16):
             match = pattern.search(sheet_ranges['A' + str(i)].value)
@@ -222,7 +222,7 @@ def parseXlsx(folder_name, tool, file, pattern, race=file):
                     lists(folder_name, tool, data_src, _spec='Random', _field='Read 4KiB (Q= 1,T= 1)', _measure=match.groupdict()['num'])   
                 elif i == 15:
                     lists(folder_name, tool, data_src, _spec='Random', _field='Write 4KiB (Q= 1,T= 1)', _measure=match.groupdict()['num'])   
-
+ 
     elif race == PCMARK_VANTAGE:
         for i in pcmark_lists:
             if i == 140:
@@ -252,12 +252,13 @@ vToday = datetime.datetime.now().strftime("%Y%m%d")
 query = ''
 path = "//10.15.202.120/das_input/Solution/SSD_Performance"
 dirs = os.listdir(path);
-folderHeaders = ['SYSTEM', 'OPERATING_SYSTEM', 'VENDOR', 'PRODUCT_NAME', 'NAND_TECH', 'CELL_TYPE','CONTROLLER', 'FORM_FACTOR', 'CAPACITY', 'FIRMWARE', 'SLC_BUFFER', 'MEASURE_DT']
+# folderHeaders = ['SYSTEM', 'OPERATING_SYSTEM', 'VENDOR', 'PRODUCT_NAME', 'NAND_TECH', 'CELL_TYPE','CONTROLLER', 'FORM_FACTOR', 'CAPACITY', 'FIRMWARE', 'SLC_BUFFER', 'MEASURE_DT']
+folderHeaders = ['MEASURE_DT','VENDOR','PRODUCT_NAME', 'CONTROLLER', 'NAND_TECH','CELL_TYPE', 'FORM_FACTOR','CAPACITY','FIRMWARE','SLC_BUFFER','SERIAL_NUMBER','TEST_COUNT']
 gFolderInfos = []
 files = []
 for subdir in dirs:
     if subdir == 'parsed':
-        break
+        continue
     pathIometer = path + '/' + subdir + '/Iometer'
     dirIometer = os.listdir(pathIometer)
     
@@ -271,23 +272,25 @@ for subdir in dirs:
 # 01.Max Throughput.csv
 for file in files:
     f = open(file['filePath'], 'r')
-    
+     
 #      d_reader = csv.DictReader(f)
 #      headers = d_reader.fieldnames
 #      for line in d_reader:
 #          matrix.append(line)
-    
+     
     #folderInfo
     folderInfos = file['folderName'].split("_")
     dicFolder = dict(zip(folderHeaders, folderInfos))
     csvReader = csv.reader(f)
     header = []
-    
+     
     for row in csvReader:
         if row[0] == "'Target Type" and len(header) == 0 :
             header = row
         if row[0] ==  "WORKER" :
             dictionary = dict(zip(header, row))
+            if dictionary["Access Specification Name"] == 'Precondition':
+                continue
             # delete ' charater
             dictionary["Target Type"] = dictionary["'Target Type"]
             del dictionary["'Target Type"]
@@ -295,65 +298,66 @@ for file in files:
             dictionary["meta"]["fileName"] = file['fileName'] #"Max Throughput.csv"
             dictionary["meta"]["folderName"] = file['folderName']
             dictionary["meta"]["tool"] = 'Iometer'
-            
+             
             #header infos
             for head in folderHeaders:
                 dictionary["meta"][head] = dicFolder[head]
-            
+             
             dictionary["keys"] = {}
             dictionary["keys"]["Access Specification Name"] = dictionary["Access Specification Name"]
             del dictionary["Access Specification Name"]
             dictionary["keys"]["Queue Depth"] = dictionary["Queue Depth"]
             del dictionary["Queue Depth"]
-            
+             
             matrix.append(dictionary)
 #             print dictionary
-      
+       
     f.close()
-    
+     
 # print len(matrix)
 try:
-    
+     
     vProduct = '' 
     con_str = 'swdashboard/swdashboard@166.125.19.98:1521/APS'
     con = cx_Oracle.connect(con_str)
     cur = con.cursor()
-    
+     
     for folders in gFolderInfos:   
         i = 0
-        
+         
         query = "delete from PERFORMANCE_FOLDER where " 
         query += " FOLDER_NAME ='" + folders['folderName'] +"'"
         cur.execute(query)
-        
+         
         query = "delete from PERFORMANCE where " 
         query += " FOLDER_NAME ='" + folders['folderName'] +"'"
-        query += " and tool = 'Iometer'"
+#         query += " and tool = 'Iometer'"
         cur.execute(query)
-        
-        query = "insert into PERFORMANCE_FOLDER(FOLDER_NAME, SYSTEM, OPERATING_SYSTEM, VENDOR, PRODUCT_NAME, NAND_TECH, CELL_TYPE, CONTROLLER, FORM_FACTOR, CAPACITY, FIRMWARE, SLC_BUFFER, MEASURE_DT)"
+         
+        query = "insert into PERFORMANCE_FOLDER(FOLDER_NAME, MEASURE_DT, VENDOR, PRODUCT_NAME, CONTROLLER, NAND_TECH, CELL_TYPE, FORM_FACTOR, CAPACITY, FIRMWARE, SLC_BUFFER, SERIAL_NUMBER, TEST_COUNT)"
         query += " values("
         query += "'" + folders['folderName'] + "'"
         for head in folderHeaders:
             query += ",'" + folders['data'][head] + "'" 
             i = i + 1
-            
+             
         query += ")"
         cur.execute(query)
-        # file move to parsed
-        
-        
+    con.commit()   
+         
+         
     i = 0
     for m in matrix:
         i = i+1
-        print i + '(' + len(matrix) + ')'
+        print repr(i) + '(' + repr(len(matrix)) + ')'
         for mKey in m: 
             if type(m[mKey]) is dict:
                 continue
             query = " insert into PERFORMANCE(FOLDER_NAME, TOOL, DATA_SRC, SPEC, SPEC_LABEL, QUEUE_DEPTH, FIELD, MEASURE)"
-            query += " values('" + m["meta"]['folderName'] +"','" + m["meta"]['tool'] +"','', '" + m["meta"]['fileName'] +"', '" + m["keys"]['Access Specification Name'] +"','', '" + m["keys"]["Queue Depth"] +"', '"+ mKey +"',  '" + m[mKey] + "'  )"
-            
+            query += " values('" + m["meta"]['folderName'] +"','" + m["meta"]['tool'] +"','" + m["meta"]['fileName'] +"', '" + m["keys"]['Access Specification Name'] +"','', '" + m["keys"]["Queue Depth"] +"', '"+ mKey +"',  '" + m[mKey] + "'  )"
+             
             cur.execute(query)
+    con.commit()
     #Iometer End
     #not Iometer
     # Tag
@@ -370,44 +374,44 @@ try:
     overallperformance_tag = '{http://www.bapco.com}data/{http://www.bapco.com}overallperformance'
     storage_score = 'results/result/storage_score'
     storage_bandwidth = 'results/result/storage_bandwidth'
-    
+     
     # Regex
     pattern = re.compile(r'((?P<num>\d*).\d+)\sMB\/s')
     pattern_txt = re.compile(r'((?P<flt>\d*.\d+))\sMB\/s')
     pattern_score = re.compile(r'(score:\s(?P<score>\d+)\s)')
     pattern_tBOOTUP = re.compile(r'tBOOTUP\s:\s(?P<por>\d+)')
-                
+                 
     pwd = '//10.15.202.120/das_input/Solution/SSD_Performance'
-
+ 
     for path, dirs, files in os.walk(pwd):
-        dirs[:] = [dir for dir in dirs if dir != "Iometer" and dirs != "parsed" ]
+        dirs[:] = [dir for dir in dirs if dir != "Iometer" and dir not in("parsed") ]
         for file in files:
             if os.path.basename(file) != 'readme.txt':
                 current_dir_path = os.path.join(path, file)             
                 path_sep = os.path.sep
                 components = current_dir_path.split(path_sep)                    
-                
+                 
                 if file == 'PCMARK_VANTAGE.xlsx':
                     PCMARK_VANTAGE = current_dir_path
                     folder_name = components[-3]
                     tool = components[-2]
                     print '----parseXlsx'
                     parseXlsx(folder_name, tool, PCMARK_VANTAGE, pattern, race=PCMARK_VANTAGE)
-                    
+                     
                 elif file == 'CDM301.xlsx':
                     CDM301 = current_dir_path
                     folder_name = components[-3]
                     tool = components[-2]        
                     print '----parseXlsx'
                     parseXlsx(folder_name, tool, CDM301, pattern, race=CDM301)   
-                    
+                     
                 elif file == 'CDM503.xlsx':
                     CDM503 = current_dir_path
                     folder_name = components[-3]
                     tool = components[-2]
                     print '----parseXlsx'
                     parseXlsx(folder_name, tool, CDM503, pattern, race=CDM503)
-                    
+                     
                 elif file == 'AS-SSD1.6.xml':
                     AS_SSDv16 = current_dir_path
                     folder_name = components[-3]
@@ -421,7 +425,7 @@ try:
                     parseXml(folder_name, tool, AS_SSDv16, Random4K64TTest_w_tag)
                     parseAccTime(folder_name, tool, AS_SSDv16, acc_time_r_tag)
                     parseAccTime(folder_name, tool, AS_SSDv16, acc_time_w_tag)
-                    
+                     
                 elif file == 'AS-SSD1.8.xml':
                     AS_SSDv18 = current_dir_path
                     folder_name = components[-3]
@@ -434,8 +438,8 @@ try:
                     parseXml(folder_name, tool, AS_SSDv18, Random4K64TTest_r_tag)
                     parseXml(folder_name, tool, AS_SSDv18, Random4K64TTest_w_tag)
                     parseAccTime(folder_name, tool, AS_SSDv18, acc_time_r_tag)
-                    parseAccTime(folder_name, tool, AS_SSDv16, acc_time_w_tag)
-                    
+                    parseAccTime(folder_name, tool, AS_SSDv18, acc_time_w_tag)
+                     
                 elif file == 'PCMARK7.txt':
                     PCMARK7 = current_dir_path
                     folder_name = components[-3]
@@ -449,7 +453,7 @@ try:
                     parseTxt(folder_name, tool, PCMARK7, 16, 15, 'Starting applications', p=pattern_txt)
                     parseTxt(folder_name, tool, PCMARK7, 15, 14, 'Gaming', p=pattern_txt)
                     parseTxt(folder_name, tool, PCMARK7, 22, 21, 'Secondary storage score', p=pattern_score)
-                    
+                     
                 elif file == 'PCMARK8.xml':
                     PCMARK8 = current_dir_path
                     folder_name = components[-3]
@@ -457,29 +461,30 @@ try:
                     print '----PCMARK8'
                     parseMark8Score(folder_name, tool, PCMARK8, storage_score)
                     parseMark8(folder_name, tool, PCMARK8, storage_bandwidth)
-                    
+                     
                 elif file == 'SYSMARK2014.fdr':
                     SYSMARK2014 = current_dir_path
                     folder_name = components[-3]
                     tool = components[-2]
                     print '----parseFdr'
                     parseFdr(folder_name, tool, SYSMARK2014, overall_tag, scenario_tag, overallperformance_tag)
-                    
+                     
                 elif file == 'POR.txt':
                     POR = current_dir_path
                     folder_name = components[-3]
                     tool = components[-2]                        
                     print '----parseTxtPor/Spor'
                     parseTxtPor(folder_name, tool, POR, pattern_tBOOTUP, spec='POR time')
-                    
+                     
                 elif file == 'SPOR.txt':
                     SPOR = current_dir_path
                     folder_name = components[-3]
                     tool = components[-2]
                     print '----parseTxtPor/Spor'
                     parseTxtPor(folder_name, tool, SPOR, pattern_tBOOTUP, spec='SPOR time')
-        
+         
     con.commit()
+    # file move to parsed
     for folders in gFolderInfos:
         src = path + '/' + folders['folderName']
         dst = path + '/parsed/' + folders['folderName']
